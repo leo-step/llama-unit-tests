1bf2ac995ebfd39cc657e4e8ac5046c1729ecc15
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2024-10-16 17:44:16 -0400

    dataset exploration

---
+++ apps_dataset_explo.py
@@ -0,0 +1,10 @@
+from datasets import load_dataset
+import json
+
+ds = load_dataset("codeparrot/apps", split="train")
+sample = next(iter(ds))
+# non-empty solutions and input_output features can be parsed from text format this way:
+sample["solutions"] = json.loads(sample["solutions"])
+sample["input_output"] = json.loads(sample["input_output"])
+print(sample)
+print(len(sample["solutions"]))
