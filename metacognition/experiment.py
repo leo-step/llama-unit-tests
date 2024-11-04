3100fd0bb9fb53d44ea4fabce4e19d9b91bb730c
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2024-11-03 22:34:37 -0500

    scaffolding

---
+++ metacognition/experiment.py
@@ -0,0 +1,6 @@
+# create experiment to evaluate bug insertion diversity
+# between usage of BugLibrary and base model when asked
+# to insert a bug into code. Diversity can be evaluated as
+# the line numbers which were modified, the type of bug
+# inserted (can be identified via GPT-prompt), other metrics
+# that I can ask ChatGPT about such as # chars changed and stuff.
---
+++ metacognition/scaffold.py
@@ -0,0 +1,8 @@
+class BugLibrary:
+    def __init__(self):
+        pass
+
+    def get_relevant_bugs(program_under_test: str):
+        # get most relevant bugs for the program
+        # return bug labels and exemplars for use in gpt prompt
+        pass
