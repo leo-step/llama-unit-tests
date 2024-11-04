df5da301463614b3dead9356c329579ec93081ff
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2024-11-03 22:36:12 -0500

    more ideas

--- metacognition/experiment.py
+++ metacognition/experiment.py
@@ -4,3 +4,10 @@
 # the line numbers which were modified, the type of bug
 # inserted (can be identified via GPT-prompt), other metrics
 # that I can ask ChatGPT about such as # chars changed and stuff.
+
+# can see if diversity improves even more if I specify which line 
+# numbers the bug needs to be inserted into specifically
+
+# then make sure things aren't trivial in the sense that the
+# default test cases don't always catch the inserted bug
+# and the program actually runs most of the time
